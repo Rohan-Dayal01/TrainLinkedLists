8ea685e4908782af8fda3f2bdeba33bdc1e83083
Rohan-Dayal01 <50881330+Rohan-Dayal01@users.noreply.github.com>
2020-03-05 22:29:32 -0500

    Update NoTrainSelectedException.java

--- NoTrainSelectedException.java
+++ NoTrainSelectedException.java
@@ -1,10 +1,5 @@
 /**
  * @author Rohan Dayal
- * @ID_Number 112768867
- * @Recitation 02
- */
-package homework2;
-/**
  * Class defines the NoTrainSelectedException and extends Exception class.
  * Should be thrown when trying to perform operation on cursor in Track
  * class, but no Train assigned to cursor
